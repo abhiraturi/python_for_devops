2b7c27389ec3c453d731ea7bb2da75ff816eb6c9
abhiraturi <abhishek.raturi6@gmail.com>
2023-09-01 20:39:23 -0400

    added python cheatsheet

---
+++ addition.py
@@ -0,0 +1,9 @@
+def addition(a,b):
+    return(a+b)
+
+num1=int(input("Enter first number "))
+num2=int(input("Enter first number "))
+add= addition(num1, num2)
+print(f"Sum of the provided two numbers is {add}")
+
+
